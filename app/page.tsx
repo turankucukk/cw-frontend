1791b201bd9b51cd2dbf7396657c84b11d3fd2f0
ilsugoymen <ilsugoymen1@gmail.com>
2026-07-16 20:51:09 +0300

    Create homepage sections and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
 import Navbar from "@/src/components/layout/Navbar";
-import RoomsSection from "@/src/components/rooms/RoomSection";
+import AboutSection from "@/src/components/home/AboutSection";
 import Hero from "@/src/components/layout/Hero";
+import HowItWorks from "@/src/components/home/HowItWorks";
+import FeaturesSection from "@/src/components/home/FeaturesSection";
+import StatisticsSection from "@/src/components/home/StatisticsSection";
+import TestimonialsSection from "@/src/components/home/TestimonailsSection";
+import TrustedSection from "@/src/components/home/TrustedSection";
+import CTASection from "@/src/components/home/CTASection";
+import Footer from "@/src/components/layout/Footer";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
-      <RoomsSection />
+      <AboutSection />
+      <HowItWorks/>
+      <FeaturesSection />
+      <StatisticsSection />
+      <TestimonialsSection />
+      <TrustedSection />
+      <CTASection />
+      <Footer />
     </main>
   );
 }
