6a15ae79c544b3fa3ef926b18d3a622dc8c6f468
ilsugoymen <ilsugoymen1@gmail.com>
2026-07-21 11:24:31 +0300

    Anasayfa eklendi

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Navbar from "@/src/components/layout/Navbar";
-import RoomsSection from "@/src/components/rooms/RoomSection";
-import Hero from "@/src/components/layout/Hero";
+import Anasayfa from "@/src/components/home/Home";
 import { Box } from "@mui/material";
+
 export default function Home() {
   return (
     <main>
       <Navbar />
        <Box sx={{ pt: '64px' }}> {/* navbar'ın yüksekliği kadar */}
-    <RoomsSection />
+      <Anasayfa />
   </Box>
       
     </main>
